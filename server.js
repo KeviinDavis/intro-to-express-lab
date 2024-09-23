c8474587554c9c108ed2a4e696dbdb40c5887607
KeviinDavis <daviskevin1@hotmail.com>
2024-09-23 09:34:44 -0400

    Q3

---
+++ server.js
@@ -0,0 +1,44 @@
+// 1. Be Polite, Greet the User
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/greetings/:username', (req, res) => {
+    const username = req.params.username;
+    res.send(`Hello there, ${username}!`);
+});
+
+// app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
+
+// //2. Rolling the Dice
+
+app.get('/roll/:number',(req, res) => {
+    const num = Number(req.params.number);
+
+    if (isNaN(num)) {
+        res.send('You must specify number.');
+    } else {
+        const randomNumber = Math.floor(Math.random() * (num + 1));
+        res.send(`You rolled a ${randomNumber}`);
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
+
+// 3. I Want THAT One!
+const collectibles = [
+    { name: 'shiny ball', price: 5.95 },
+    { name: 'autographed picture of a dog', price: 10 },
+    { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+  ];
+
+  app.get('/collectibles/:index', (req, res) => {
+    const index = Number(req.params.index);
+  });
+
+  
